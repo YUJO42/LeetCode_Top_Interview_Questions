2c7549bf91e1a574190575972fd42843f1ce41ee
yujo <yujo@student.42seoul.kr>
2020-07-17 13:16:49 +0900

    2020 07 17 daily ps [TS]String

--- EASY/Array/Rotate Image.ts
+++ EASY/Array/Rotate Image.ts
@@ -2,24 +2,39 @@
  Do not return anything, modify matrix in-place instead.
  */
 function rotate(matrix: number[][]): void {
-  //
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i + 1; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < Math.floor(matrix.length / 2); j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[i][matrix[i].length - j - 1];
+      matrix[i][matrix[i].length - j - 1] = temp;
+    }
+  }
 }
 
 // test code
 
-console.log(
-  rotate([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ])
-);
+const test1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
-console.log(
-  rotate([
-    [5, 1, 9, 11],
-    [2, 4, 8, 10],
-    [13, 3, 6, 7],
-    [15, 14, 12, 16],
-  ])
-);
+const test2 = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
+
+rotate(test1);
+rotate(test2);
+
+console.log(test1);
+console.log(test2);
---
+++ EASY/Strings/Longest Common Prefix.ts
@@ -0,0 +1,23 @@
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0) {
+    return "";
+  }
+
+  let prefix: string = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix.length === 0) {
+        return "";
+      }
+    }
+  }
+
+  return prefix;
+}
+
+// test code
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // output : "fl"
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); // output : ""
