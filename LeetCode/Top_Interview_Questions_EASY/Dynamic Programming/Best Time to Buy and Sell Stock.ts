102571c2208cf71e8d16d761b0870ade2c2edd54
yujo <yujo@student.42seoul.kr>
2020-07-31 15:14:50 +0900

    2020 07 31 [TS]Dynamic Programming

---
+++ LeetCode/Top_Interview_Questions_EASY/Dynamic Programming/Best Time to Buy and Sell Stock.ts
@@ -0,0 +1,17 @@
+function maxProfit(prices: number[]): number {
+  let min: number = 2147483647;
+  let profit: number = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    } else if (prices[i] - min > profit) {
+      profit = prices[i] - min;
+    }
+  }
+  return profit;
+}
+
+// test code
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
+console.log(maxProfit([7, 6, 4, 3, 1])); // 0
