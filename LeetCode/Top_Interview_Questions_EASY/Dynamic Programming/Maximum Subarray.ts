bf745e5a6dff63d82a470e177baeb33115d693a5
yujo <yujo@student.42seoul.kr>
2020-08-01 12:10:44 +0900

    2020 08 01 daily ps [TS]Dynamic Programming

--- LeetCode/Top_Interview_Questions_EASY/Dynamic Programming/Maximum Subarray.ts
+++ LeetCode/Top_Interview_Questions_EASY/Dynamic Programming/Maximum Subarray.ts
@@ -1,3 +1,15 @@
 function maxSubArray(nums: number[]): number {
-  //
+  let sum: number = nums[0];
+  let maxSum: number = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    sum = Math.max(nums[i], sum + nums[i]);
+    maxSum = Math.max(maxSum, sum);
+  }
+
+  return maxSum;
 }
+
+// test code
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
