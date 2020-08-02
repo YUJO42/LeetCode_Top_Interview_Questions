3f97d0bdda00ff784979e8beab7b623b498fb6e9
yujo <yujo@student.42seoul.kr>
2020-08-02 12:07:38 +0900

    change laptop

---
+++ LeetCode/2020_August_LeetCoding_Challenge/week01_Detect_Capital.ts
@@ -0,0 +1,17 @@
+// a ~ z = 97 ~ 122
+// A ~ Z = 65 ~ 90
+
+function detectCapitalUse(word: string): boolean {
+	let flag : boolean = true;
+	let checkCapital : boolean = word[0].charCodeAt(0) <= 90 ? true : false;
+	for (let i = 1; i < word.length; i++) {
+		if (checkCapital) {
+			//
+		}else {
+			if (word[i].charCodeAt > )
+		}
+	}
+
+	return true;
+};
+
