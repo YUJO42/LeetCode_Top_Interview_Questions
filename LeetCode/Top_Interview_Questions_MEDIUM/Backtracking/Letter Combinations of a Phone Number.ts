02da2621130633e142de862ad4b491c8792e518c
yujo <yujo@student.42seoul.kr>
2020-08-18 12:45:23 +0900

    2020 08 18 daily ps [JS]Backtracking

--- LeetCode/Top_Interview_Questions_MEDIUM/Backtracking/Letter Combinations of a Phone Number.ts
+++ LeetCode/Top_Interview_Questions_MEDIUM/Backtracking/Letter Combinations of a Phone Number.ts
@@ -1,3 +1,36 @@
 function letterCombinations(digits: string): string[] {
-  // /
+  if (!digits.length) return [];
+
+  const arr: string[][] = [
+    [],
+    [],
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    ['j', 'k', 'l'],
+    ['m', 'n', 'o'],
+    ['p', 'q', 'r', 's'],
+    ['t', 'u', 'v'],
+    ['w', 'x', 'y', 'z'],
+  ];
+  const result: string[] = [];
+
+  function premute(str, idx) {
+    if (idx === digits.length) {
+      result.push(str);
+      return;
+    }
+
+    for (let alpha of arr[digits[idx]]) {
+      premute(str + alpha, idx + 1);
+    }
+  }
+
+  premute('', 0);
+
+  return result;
 }
+
+// test code
+
+console.log(letterCombinations('23'));
