df0a400e9175083a2826b2d4b1fa8908708cb9a0
yujo <yujo@student.42seoul.kr>
2020-09-02 08:33:20 +0900

    2020 09 02 daily ps [TS]Dynamic Programming

---
+++ LeetCode/Top_Interview_Questions_MEDIUM/Dynamic Programming/Coin Change.ts
@@ -0,0 +1,17 @@
+function coinChange(coins: number[], amount: number): number {
+  const dp: number[] = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (let coin of coins) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
+
+// test code
+
+console.log(coinChange([1, 2, 5], 11));
+console.log(coinChange([2], 3));
