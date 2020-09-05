45a75b587d4c1a6816dde9eba3e04f40c1a4bda1
yujo <yujo@student.42seoul.kr>
2020-09-06 01:12:13 +0900

    2020 09 06 daily ps [JS]Math

---
+++ LeetCode/Top_Interview_Questions_MEDIUM/Math/Happy Number.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+  var map = {};
+  var tmp = 0;
+
+  if (n < 1) return false;
+
+  while (n !== 1 && !map[n]) {
+    map[n] = true;
+    tmp = 0;
+
+    while (n > 0) {
+      tmp += Math.pow(n % 10, 2);
+      n = Math.floor(n / 10);
+    }
+
+    n = tmp;
+    // console.log(map);
+  }
+
+  return n === 1;
+};
+
+// test code
+
+console.log(isHappy(19));
