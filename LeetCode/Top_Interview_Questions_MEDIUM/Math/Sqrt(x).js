a06b4ce1ed4077cb80b8522ddb7064f1864ff012
yujo <yujo@student.42seoul.kr>
2020-09-10 08:42:27 +0900

    2020 09 10 daily ps [JS]Math

---
+++ LeetCode/Top_Interview_Questions_MEDIUM/Math/Sqrt(x).js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  return Math.floor(Math.sqrt(x));
+};
+
+// test code
+
+console.log(mySqrt(4));
+console.log(mySqrt(2));
+console.log(mySqrt(8));
