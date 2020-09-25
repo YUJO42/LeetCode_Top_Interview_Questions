7f13224e24900a7e110636c617bed4093b4c7e16
yujo <yujo@student.42seoul.kr>
2020-09-26 07:59:16 +0900

    2020 09 26 daily ps [TS]Array

--- LeetCode/Top_Interview_Questions_HARD/Array and Strings/First Missing Positive.ts
+++ LeetCode/Top_Interview_Questions_HARD/Array and Strings/First Missing Positive.ts
@@ -1,5 +1,23 @@
 function firstMissingPositive(nums: number[]): number {
-  //
+  let i: number = 0;
+
+  while (i < nums.length) {
+    if (
+      nums[i] > 0 &&
+      nums[i] <= nums.length &&
+      nums[nums[i] - 1] !== nums[i]
+    ) {
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+    } else {
+      i++;
+    }
+  }
+
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) return i + 1;
+  }
+
+  return i + 1;
 }
 
 // test
