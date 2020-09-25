1c3ff2ad60dd8e6d3d50a2c673117dafb8b6e6a5
yujo <yujo@student.42seoul.kr>
2020-09-25 14:18:43 +0900

    2020 09 25 daily ps [TS]Fail...

---
+++ LeetCode/Top_Interview_Questions_HARD/Array and Strings/First Missing Positive.ts
@@ -0,0 +1,9 @@
+function firstMissingPositive(nums: number[]): number {
+  //
+}
+
+// test
+
+console.log(firstMissingPositive([1, 2, 0]));
+console.log(firstMissingPositive([3, 4, -1, 1]));
+console.log(firstMissingPositive([7, 8, 9, 11, 12]));
---
+++ LeetCode/Top_Interview_Questions_HARD/Array and Strings/Game of Life.ts
@@ -0,0 +1,26 @@
+/**
+ Do not return anything, modify board in-place instead.
+ */
+function gameOfLife(board: number[][]): void {
+  //
+}
+
+// test code
+
+console.log(
+  gameOfLife([
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ]),
+);
+
+/* output
+[
+  [0,0,0],
+  [1,0,1],
+  [0,1,1],
+  [0,1,0]
+]
+*/
