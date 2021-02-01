a79ede87c24d50c00ebf6dee2edb0b6dcfec58ee
yujo <yujo@student.42seoul.kr>
2021-02-02 07:51:09 +0900

    feat: 217. Contains Duplicate solution

---
+++ new-year-gift/Array/Contains-Duplicate.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = (nums) => {
+  const map = new Map();
+
+  for (const num of nums) {
+    if (map.has(num)) {
+      return true;
+    }
+    map.set(num, 1);
+  }
+
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
