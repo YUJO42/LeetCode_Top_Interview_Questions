a1fa51d7234b5020d749000382086c8b3e1f82bb
yujo <yujo@student.42seoul.kr>
2021-02-02 10:17:20 +0900

    feat: 217. Contains Duplicate solution 2

--- new-year-gift/Array/Contains-Duplicate.js
+++ new-year-gift/Array/Contains-Duplicate.js
@@ -2,6 +2,12 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+/* 
+  SUCCESS 
+   Runtime : 84ms 
+   Memory  : 46MB
+*/
 const containsDuplicate = (nums) => {
   const map = new Map();
 
@@ -10,6 +16,22 @@ const containsDuplicate = (nums) => {
       return true;
     }
     map.set(num, 1);
+  }
+
+  return false;
+};
+
+/* 
+  SUCCESS 
+   Runtime : 1860ms 
+   Memory  : 46MB
+*/
+const containsDuplicate2 = (nums) => {
+  while (nums.length > 1) {
+    const currentNumber = nums.shift();
+    if (nums.includes(currentNumber)) {
+      return true;
+    }
   }
 
   return false;
